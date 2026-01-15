cc899e0ead9891bc66d2c94e667898c070a29b08
youBlush <86181929+youBlush@users.noreply.github.com>
2026-01-15 13:27:16 +0800

    Update leaflow_checkin.py

--- leaflow_checkin.py
+++ leaflow_checkin.py
@@ -14,6 +14,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import TimeoutException
 import requests
 from datetime import datetime
 
@@ -640,3 +641,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
